f95a1e958dedab744ab869d7fe3e9a514f3e7f2a
daniel <albertagoya@gmail.com>
2022-12-06 12:33:01 +0300

    making changes to home folder

--- home/views.py
+++ home/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from django.template import loader
+
+
+
+
+from django.db.models import Count,F,Sum,Avg
+from django.db.models.functions import ExtractYear,ExtractMonth
+from kilimodata.models import *
+# from .utils import (
+#     months,colorDanger,
+#     colorPrimary,colorSuccess,
+#     generate_color_palette,get_year_dict)
+# Create your views here.
+
 
 
 def index(request):
@@ -10,6 +23,74 @@ def index(request):
         
     }
     return HttpResponse(template.render(context, request))
+
+def about(request):
+    
+    template = loader.get_template('home/about/about.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+def faq(request):
+    
+    template = loader.get_template('home/about/faq.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def terms(request):
+    
+    template = loader.get_template('home/resources/terms.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+def policy(request):
+    
+    template = loader.get_template('home/resources/policy.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def census(request):
+    
+    template = loader.get_template('home/resources/census.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+def definitions(request):
+    
+    template = loader.get_template('home/resources/definitions.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+def metadata(request):
+    
+    template = loader.get_template('home/resources/metadata.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+def covid19(request):
+    
+    template = loader.get_template('home/blog/covid19.html')
+    context = {
+        
+    }
+    return HttpResponse(template.render(context, request))
+
+
 
 
 def datadownload(request):
@@ -21,11 +102,3 @@ def datadownload(request):
     return HttpResponse(template.render(context, request))
 
 
-
-def covid19(request):
-    
-    template = loader.get_template('home/blog/covid19.html')
-    context = {
-        
-    }
-    return HttpResponse(template.render(context, request))
